b0962b8d806a2753478a45cc0e31ba318fe40efc
yisenlin <ysl.130@qq.com>
2023-02-15 00:57:12 +0800

    update

--- data/__init__.py
+++ data/__init__.py
@@ -2,8 +2,10 @@ from .llff import LLFFDataset
 from .blender import BlenderDataset
 from .dtu_ft import DTU_ft
 from .dtu import MVSDatasetDTU
+from .avt import AvtDataset
 
 dataset_dict = {'dtu': MVSDatasetDTU,
                 'llff':LLFFDataset,
                 'blender': BlenderDataset,
+                'avt': AvtDataset,
                 'dtu_ft': DTU_ft}
